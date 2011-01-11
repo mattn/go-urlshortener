34cdab789f58efbf948efb0f4ad7bc553b729ea9
mattn <mattn.jp@gmail.com>
2011-01-11 14:41:41 +0900

    analytics

--- urlshorter.go
+++ urlshorter.go
@@ -50,7 +50,57 @@ func ExpandURL(shortUrl string) (expandedUrl string, err os.Error) {
 	buf.Write(b)
 	dec := json.NewDecoder(&buf)
 	var out map[string]interface{}
-	dec.Decode(&out)
+	err = dec.Decode(&out)
+	if err != nil {
+		return
+	}
 	expandedUrl = out["longUrl"].(string)
+	return
+}
+
+type AnalyticsCount struct {
+    Count string
+    Id string
+}
+
+type AnalyticsItem struct {
+	ShortUrlClicks string
+	LongUrlClicks string
+	Referrers []AnalyticsCount
+	Countries []AnalyticsCount
+	Browsers []AnalyticsCount
+	Platforms []AnalyticsCount
+}
+
+type AnalyticsInfo struct {
+ Kind string
+ Id string
+ LongUrl string
+ Status string
+ Created string
+ Analytics struct {
+  AllTime AnalyticsItem
+  Month AnalyticsItem
+  Week AnalyticsItem
+  Day AnalyticsItem
+  TwoHours AnalyticsItem
+ }
+}
+
+func AnalyticsURL(shortUrl string) (info AnalyticsInfo, err os.Error) {
+	param := http.EncodeQuery(map[string][]string{"shortUrl": {shortUrl}, "projection": {"FULL"}})
+	res, _, err := http.Get("https://www.googleapis.com/urlshortener/v1/url?" + param)
+	if err != nil {
+		return
+	}
+	if res.StatusCode != 200 {
+		err = os.NewError("failed to post")
+		return
+	}
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(b, &info)
 	return
 }
