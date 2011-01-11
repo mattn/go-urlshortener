e65a62044c383522199e0e2cf20e5fa899893951
mattn <mattn.jp@gmail.com>
2011-01-11 15:50:50 +0900

    move analytics to shorten

--- example/shorten.go
+++ example/shorten.go
@@ -1,15 +1,48 @@
 package main
 
 import "google/urlshortener"
+import "bytes"
 import "flag"
 import "fmt"
 import "os"
+import "template"
 
 var expand = flag.Bool("e", false, "expand URL")
+var info = flag.Bool("i", false, "show info about URL")
+
+func analytics(a urlshortener.AnalyticsInfo) {
+	var b bytes.Buffer
+	tmpl, _ := template.Parse(`
+Kind: {Kind}
+Id: {Id}
+LongUrl: {LongUrl}
+Status: {Status}
+Created: {Created}
+AllTime:
+{.section Analytics.AllTime}
+  ShortUrlClicks: {ShortUrlClicks}
+  LongUrlClicks: {LongUrlClicks}
+  Referrers:
+{.repeated section Referrers}    {Id} ({Count})
+{.end}
+  Countries:
+{.repeated section Countries}    {Id} ({Count})
+{.end}
+  Browsers:
+{.repeated section Browsers}    {Id} ({Count})
+{.end}
+  Platforms:
+{.repeated section Platforms}    {Id} ({Count})
+{.end}
+{.end}
+`, nil)
+	tmpl.Execute(a, &b)
+	println(b.String())
+}
 
 func main() {
 	flag.Usage = func() {
-		fmt.Fprint(os.Stderr, "usage: urlshortener [-e] URL\n")
+		fmt.Fprint(os.Stderr, "usage: urlshortener [-e|-i] URL\n")
 		os.Exit(2)
 	}
 	flag.Parse()
@@ -18,16 +51,26 @@ func main() {
 		flag.Usage()
 	}
 
-	var u string
-	var e os.Error
 	if *expand {
-		u, e = urlshorter.ExpandURL(args[0])
+		u, e := urlshortener.ExpandURL(args[0])
+		if e != nil {
+			panic(e.String())
+		} else {
+			println(u)
+		}
+	} else if *info {
+		a, e := urlshortener.AnalyticsURL(args[0])
+		if e != nil {
+			panic(e.String())
+		} else {
+			analytics(a)
+		}
 	} else {
-		u, e = urlshorter.ShortenURL(args[0])
-	}
-	if e != nil {
-		panic(e.String())
-	} else {
-		println(u)
+		u, e := urlshortener.ShortenURL(args[0])
+		if e != nil {
+			panic(e.String())
+		} else {
+			println(u)
+		}
 	}
 }
